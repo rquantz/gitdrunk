a8766b7b7b3e0387b0a74d27edef6fcd3708bfe8
Rick Quantz <rquantz@gmail.com>
2013-05-03 20:17:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cocktails::Application.config.secret_key_base = 'f2eff089cff0e6846733f067ddc3b243a10cb30af2123e5f801796fcca7f257d6ef87c9a9eabd30ec1ecad076c6dbf5977e6979fa51f114063cd6df19d2054a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cocktails::Application.config.session_store :cookie_store, key: '_cocktails_session'
