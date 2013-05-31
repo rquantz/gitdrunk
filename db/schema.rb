5b310327422e5ff1fc4b1ff65e4c0a2b1090cc03
Rick Quantz <rquantz@gmail.com>
2013-05-31 16:43:56 -0400

    Added username to Users. Permit username for sign_in.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130530220406) do
+ActiveRecord::Schema.define(version: 20130531204147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20130530220406) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
